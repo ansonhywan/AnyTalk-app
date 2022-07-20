3d920efd607674164c8af14c1166de410c368b9c
ahtchow <54782402+ahtchow@users.noreply.github.com>
2022-07-20 00:03:03 -0400

    api - t2s, s2t demo

---
+++ api/speech-to-text/GCP_S2T_demo.py
@@ -0,0 +1,26 @@
+# Imports the Google Cloud client library
+from google.cloud import speech
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'anytalk-gcp-cred.json'
+
+
+# Instantiates a client
+client = speech.SpeechClient()
+
+# The name of the audio file to transcribe
+gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+
+audio = speech.RecognitionAudio(uri=gcs_uri)
+
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code="en-US",
+)
+
+# Detects speech in the audio file
+response = client.recognize(config=config, audio=audio)
+
+for result in response.results:
+    print(result)
+    print("Transcript: {}".format(result.alternatives[0].transcript))
