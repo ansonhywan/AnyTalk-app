0a3ba5440f80d1abebc0cb772d9bfb566375ce9b
ahtchow <54782402+ahtchow@users.noreply.github.com>
2022-07-23 18:47:43 -0400

    GCP utils init

--- backend/GCP_utils.py
+++ backend/GCP_utils.py
@@ -1,15 +1,44 @@
-from google.cloud import texttospeech, speech, storage
+from google.cloud import texttospeech, speech
+import json
+from datetime import datetime
+
 
 class GCP_utils:
+    '''
+    Utility object to interact with GCP Cloud ServicesAPI
+    '''
     def __init__(self):
-        self.storage_client = storage.Client.from_service_account_json(
-            'creds.json')
+        # self.storage_client = storage.Client.from_service_account_json('creds.json')
+        self.t2s_client = texttospeech.TextToSpeechClient()
+        self.s2t_client = speech.SpeechClient()
 
-    def convert_t2s(self):
-        pass
+    def convert_t2s(self, text, gender="male"):
+        synthesis_input = texttospeech.SynthesisInput(text=text)
+        voice_name = "en-US-Wavenet-D" if gender == 'male' else "en-US-Wavenet-E"
+        voice_config = texttospeech.VoiceSelectionParams(language_code="en-US",
+                                                         name=voice_name)
+        audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
+        response = self.t2s_client.synthesize_speech(input=synthesis_input, 
+                                                     voice=voice_config, 
+                                                     audio_config=audio_config)
+        timestr = datetime.now().strftime("%Y%m%d-%H%M%S")
+        speech_dir = timestr + '.mp3'
+        with open(speech_dir, "wb") as speech_mp3:
+            speech_mp3.write(response.audio_content)
+        return speech_dir
 
-    def convert_s2t(self):
-        pass
+    def convert_s2t(self, speech_uri):
+        audio = speech.RecognitionAudio(uri=speech_uri)
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=16000,
+            language_code="en-US",
+        )
+        response = self.s2t_client.recognize(config=config, audio=audio)
+        text = ""
+        for result in response.results:
+            text = text + result.alternatives[0].transcript + " "
+        return text
 
     def upload_file(self):
         print(buckets = list(self.storage_client.list_buckets()))
@@ -17,3 +46,5 @@ class GCP_utils:
         blob = bucket.blob(blob_name)
         blob.upload_from_filename(path_to_file)
         return blob.public_url 
+
+import pdb; pdb.set_trace()
