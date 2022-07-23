84d130c29d712f8ca05e5424a02f9117c551a2a8
alexjubs <redpod22@gmail.com>
2022-07-23 16:41:14 -0400

    gcp file upload

--- backend/GCP_utils.py
+++ backend/GCP_utils.py
@@ -1,14 +1,19 @@
-from google.cloud import texttospeech, speech
+from google.cloud import texttospeech, speech, storage
 
 class GCP_utils:
-    def __init__():
+    def __init__(self):
+        self.storage_client = storage.Client.from_service_account_json(
+            'creds.json')
+
+    def convert_t2s(self):
         pass
 
-    def convert_t2s():
+    def convert_s2t(self):
         pass
 
-    def convert_s2t():
-        pass
-
-    def upload_file():
-        pass
+    def upload_file(self):
+        print(buckets = list(self.storage_client.list_buckets())
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.upload_from_filename(path_to_file)
+        return blob.public_url 
