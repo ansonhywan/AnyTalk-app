5f8714f5b6e1b3c16c4341609839f9f80f29ad8f
alexjubs <redpod22@gmail.com>
2022-07-23 22:28:07 -0400

    linear16 encoding

--- backend/GCP_utils.py
+++ backend/GCP_utils.py
@@ -15,12 +15,12 @@ class GCP_utils:
         voice_name = "en-US-Wavenet-D" if gender == 'male' else "en-US-Wavenet-E"
         voice_config = texttospeech.VoiceSelectionParams(language_code="en-US",
                                                          name=voice_name)
-        audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
+        audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.LINEAR16)
         response = self.t2s_client.synthesize_speech(input=synthesis_input, 
                                                      voice=voice_config, 
                                                      audio_config=audio_config)
         timestr = datetime.now().strftime("%Y%m%d-%H%M%S")
-        speech_dir = timestr + '.mp3'
+        speech_dir = timestr + '.wav'
         with open(speech_dir, "wb") as speech_mp3:
             speech_mp3.write(response.audio_content)
         return speech_dir
