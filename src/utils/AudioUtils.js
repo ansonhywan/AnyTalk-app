43ce3add3479655f94662164ed17af15fda9cd81
Anson Wan <anson.hy.wan@gmail.com>
2022-07-23 21:12:28 -0400

    Working recording and playback.

---
+++ src/utils/AudioUtils.js
@@ -0,0 +1,32 @@
+export async function onStartRecord(audioRecorderPlayer) {
+    console.log('onStartRecord');
+    const result = await audioRecorderPlayer.startRecorder();
+    audioRecorderPlayer.addRecordBackListener(e => {
+        console.log('Recording . . . ', e.currentPosition);
+        return;
+    });
+    return result;
+}
+
+export async function onStopRecord(audioRecorderPlayer) {
+    console.log('onStopRecord');
+    const result = await audioRecorderPlayer.stopRecorder();
+    audioRecorderPlayer.removeRecordBackListener();
+    // console.log(result);
+}
+
+export async function onStartPlay(audioRecorderPlayer) {
+    console.log('onStartPlay');
+    const msg = await audioRecorderPlayer.startPlayer();
+    // console.log(msg);
+}
+
+export async function onPausePlay(audioRecorderPlayer) {
+    await audioRecorderPlayer.pausePlayer();
+}
+
+export async function onStopPlay(audioRecorderPlayer) {
+    console.log('onStopPlay');
+    audioRecorderPlayer.stopPlayer();
+    audioRecorderPlayer.removePlayBackListener();
+}
