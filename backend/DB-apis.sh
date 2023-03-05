0fdf6158e5d6c13b0cf582aee3164d2cea628e70
alexjubs <redpod22@gmail.com>
2023-03-04 22:48:18 -0500

    added database compatibility

---
+++ backend/DB-apis.sh
@@ -0,0 +1,8 @@
+# ----------------- ADD MESSAGE -----------------
+curl -X POST -H "Content-Type: application/json" -d '{"text":"hello there"}' 127.0.0.1:5050/messages/
+
+# ----------------- GET ALL MESSAGES -----------------
+curl -X GET http://127.0.0.1:5050/messages/
+
+# ----------------- DELETE ALL MESSAGES -----------------
+curl -X DELETE http://127.0.0.1:5050/messages/
--- backend/main.py
+++ backend/main.py
@@ -1,36 +1,85 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import json
 
 from GCP_utils import GCP_utils
 
 app = Flask(__name__)
 gcp_controller = GCP_utils()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///AnyTalk.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route('/text_to_speech/', methods=['GET', 'POST'])
+# ----------------- Database -----------------
+
+db = SQLAlchemy(app)
+
+
+class Message(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.Text)
+	message_type = db.Column(db.Text)
+
+	def __init__(self, text, message_type=None):
+		self.text = text
+		self.message_type = message_type
+
+	def to_dict(self):
+		return {"id": self.id, "text": self.text}
+
+
+db.create_all()
+
+
+@app.route("/messages/", methods=["POST"])
+def create_message():
+	return save_message(request.get_json().get("text"))
+
+@app.route("/messages/", methods=["GET"])
+def get_all_messages():
+	return jsonify(list(map(lambda x: x.to_dict(), Message.query.all())))
+
+@app.route("/messages/", methods=["DELETE"])
+def clear_all_messages():
+	db.session.query(Message).delete()
+	db.session.commit()
+	return jsonify("deleted"), 204
+
+# ----------------- API routing -----------------
+
+@app.route("/text_to_speech/", methods=["GET", "POST"])
 def text_to_speech():
 	text = request.json.get("text")
-	if text == None: return json.dumps({"error": "must specify text in body"})
+	if text == None:
+		return json.dumps({"error": "must specify text in body"})
+	save_message(text)
 
 	local_path = gcp_controller.convert_t2s(text)
 	remote_path = gcp_controller.upload_file(local_path)
+	return json.dumps({"speech_path": remote_path, "error": None})
 
-	return json.dumps({
-		"speech_path": remote_path,
-		"error": None
-	})
 
-@app.route('/speech_to_text/', methods=['GET', 'POST'])
+@app.route("/speech_to_text/", methods=["GET", "POST"])
 def speech_to_text():
 	speech_path = request.json.get("speech_path")
-	print("{}{}".format(50*"=", speech_path))
-	if speech_path == None: return json.dumps({"error": "must specify speech path in body"})
-	if not speech_path.endswith(".flac"): return json.dumps({"error": "speech file must end with .flac"})
+	print("{}{}".format(50 * "=", speech_path))
+	if speech_path == None:
+		return json.dumps({"error": "must specify speech path in body"})
+	if not speech_path.endswith(".flac"):
+		return json.dumps({"error": "speech file must end with .flac"})
 	text = gcp_controller.convert_s2t(speech_path)
-	return json.dumps({
-		"text": text,
-		"error": None
-	})
+	save_message(text)
+	return json.dumps({"text": text, "error": None})
 
-@app.route('/')
+
+@app.route("/")
 def main():
 	return "this is anytalk backend \n"
+
+# ----------------- Extras -----------------
+
+def save_message(text):
+	print(f"saving message in database: {text}")
+	new_message = Message(text)
+	db.session.add(new_message)
+	db.session.commit()
+	return jsonify(new_message.to_dict()), 201
